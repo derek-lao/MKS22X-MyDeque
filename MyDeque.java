35bae3d5331b5f676ba41ceaaaedc0dfe80f06f2
Derek Lao <dlao00@stuy.edu>
2019-03-22 09:51:38 -0400

    added some exception throwing

--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -12,16 +14,46 @@ public class MyDeque<E>{
     data = (E[])new Object[initialCapacity];
   }
 
+  boolean isEmpty = (size == 0);
 
+  private void resize(){
+    if(size == data.length)
+    {
+      if(start == end + 1)
+      {
+        @SuppressWarnings("unchecked")
+        E[] thingie = (E[]) new Object[(data.length + 1) * 2];
+        int increment = 0;
+        for(int s = start; s < data.length; s ++, increment ++)
+        {
+          thingie[increment] = data[s];
+        }
+        for(int e = 0; e < end + 1; e ++, increment ++)
+        {
+          thingie[increment] = data[e];
+        }
+        data = thingie;
+        start = 0;
+        end = increment;
+      }
+      else if(start == end) // should only happen if initial array had only one element;
+      {
+        @SuppressWarnings("unchecked")
+        E[] thingie = (E[]) new Object[(data.length + 1) * 2];
+        data[0] = data[start];
+      }
+    }
+  }
   public int size(){
     return size;
   }
   public String toString(){
-    String answer = "[";
+    String answer = "{";
     if(start > end)
     {
       for(int i = start; i < end; i ++)
       {
+        if(data[i] != null)
         answer += data[i] + ",";
       }
     }
@@ -29,21 +61,121 @@ public class MyDeque<E>{
     {
       for(int i = start; i < data.length; i ++)
       {
+        if(data[i] != null)
         answer += data[i] + ",";
       }
-      for(int i = 0; i < e; i ++)
+      for(int i = 0; i < end; i ++)
       {
+        if(data[i] != null)
         answer += data[i] + ",";
       }
     }
-    answer += data[end] + "]";
+    if(data[data.length - 1] != null)
+    {
+      answer += data[end];
+    }
+    answer += "}";
+    return answer;
   }
 
 
-  public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(E element){ }
-  public E removeLast(E element){ }
-  public E getFirst(E element){ }
-  public E getLast(E element){ }
+  public void addFirst(E element){
+    if(element == null)
+    throw new NullPointerException();
+    else
+    {
+      resize();
+      if(start != 0)
+      {
+        start --;
+        data[start] = element;
+        size ++;
+      }
+      else
+      {
+        start = data.length - 1;
+        data[start] = element;
+        size ++;
+      }
+      resize();
+    }
+  }
+  public void addLast(E element){
+    if(element == null)
+    throw new NullPointerException();
+    else
+    {
+      resize();
+      if(end != data.length - 1)
+      {
+        end ++;
+        data[end] = element;
+        size ++;
+      }
+      else
+      {
+        end = 0;
+        data[end] = element;
+        size ++;
+      }
+      resize();
+    }
+  }
+  public E removeFirst(){
+    if(!isEmpty)
+    {
+      int holder = start;
+      if(start == data.length - 1)
+      {
+        start = 0;
+        size --;
+      }
+      else
+      {
+        start ++;
+        size --;
+      }
+      return data[holder];
+    }
+    // return null; // if no elements in list
+    else
+    {
+      throw new NoSuchElementException();
+    }
+
+  }
+  public E removeLast(){
+    if(!isEmpty)
+    {
+      int holder = end;
+      if(end == 0)
+      {
+        end = data.length - 1;
+        size --;
+      }
+      else
+      {
+        end --;
+        size --;
+      }
+      return data[holder];
+    }
+    // return null; // if list is empty
+    else
+    {
+      throw new NoSuchElementException();
+    }
+  }
+  public E getFirst(){
+    if(data[start] != null)
+    return data[start];
+    else
+    throw new NoSuchElementException();
+  }
+  public E getLast(){
+    if(data[end] != null)
+    return data[end];
+    else
+    throw new NoSuchElementException();
+  }
 }
