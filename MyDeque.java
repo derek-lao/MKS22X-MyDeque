1cdcc23f880a9fb35226f8cc64c7ade4f8c2f066
Derek Lao <dlao00@stuy.edu>
2019-03-27 23:53:15 -0400

    fixed some stuff, i guess

--- MyDeque.java
+++ MyDeque.java
@@ -21,16 +21,16 @@ public class MyDeque<E>{
   }
 
   private void resize(){
-    System.out.println("Resize call began! Your start is " + start + " and your end is " + end);
-    System.out.println("Before we proceed, your data array is: " + Arrays.toString(data));
+    // System.out.println("Resize call began! Your start is " + start + " and your end is " + end);
+    // System.out.println("Before we proceed, your data array is: " + Arrays.toString(data));
     if(size == data.length)
     {
-      System.out.println("The requirement for resize has been met!");
+      // System.out.println("The requirement for resize has been met!");
       @SuppressWarnings("unchecked")
       E[] thingie = (E[]) new Object[(data.length + 1) * 2];
       if(start == end + 1)
       {
-        System.out.println("If you got here, it means that your start index was after end");
+        // System.out.println("If you got here, it means that your start index was after end");
         int increment = 0;
         for(int s = start; s < data.length; s ++, increment ++)
         {
@@ -42,11 +42,11 @@ public class MyDeque<E>{
         }
         data = thingie;
         start = 0;
-        end = increment;
+        end = increment - 1;
       }
       else if(start == end) // should only happen if initial array had only one element;
       {
-        System.out.println("If you got here, it means that your start index equal to the end");
+        // System.out.println("If you got here, it means that your start index equal to the end");
         thingie[0] = data[start];
         data = thingie;
         start = 0;
@@ -54,29 +54,29 @@ public class MyDeque<E>{
       }
       else if(start < end) // happens if you had added your numbers to your array in order from index 0 to index data.length - 1
       {
-        System.out.println("If you got here, it means that your start index before the end");
-        System.out.println("Before we proceed, here is your data array:    " + Arrays.toString(data));
-        System.out.println("Before we proceed, here is your thingie array: " + Arrays.toString(thingie));
-        System.out.println("We must print all our data values: ");
-        System.out.println("data[0] is " + data[0]);
-        System.out.println("data[1] is " + data[1]);
-        System.out.println("data[2] is " + data[2]);
+        // System.out.println("If you got here, it means that your start index before the end");
+        // System.out.println("Before we proceed, here is your data array:    " + Arrays.toString(data));
+        // System.out.println("Before we proceed, here is your thingie array: " + Arrays.toString(thingie));
+        // System.out.println("We must print all our data values: ");
+        // System.out.println("data[0] is " + data[0]);
+        // System.out.println("data[1] is " + data[1]);
+        // System.out.println("data[2] is " + data[2]);
         for(int s = 0; s < data.length; s ++)
         {
           thingie[s] = data[s];
-          System.out.println("We must print all our data values within our loop: ");
-          System.out.println("data[0] is " + data[0]);
-          System.out.println("data[1] is " + data[1]);
-          System.out.println("data[2] is " + data[2]);
-          System.out.println("s is " + s + " and data[s] is " + data[s]);
-          System.out.println("Things are failing, printing data array:   " + Arrays.toString(data));
-          System.out.println("Things are failing, printing thingie array: " + Arrays.toString(thingie));
+          // System.out.println("We must print all our data values within our loop: ");
+          // System.out.println("data[0] is " + data[0]);
+          // System.out.println("data[1] is " + data[1]);
+          // System.out.println("data[2] is " + data[2]);
+          // System.out.println("s is " + s + " and data[s] is " + data[s]);
+          // System.out.println("Things are failing, printing data array:   " + Arrays.toString(data));
+          // System.out.println("Things are failing, printing thingie array: " + Arrays.toString(thingie));
         }
         data = thingie;
       }
     }
-    System.out.println("Resize call ended! Your start is " + start + " and your end is " + end);
-    System.out.println("Your start value is " + data[start] + " and your end value is " + data[end]);
+    // System.out.println("Resize call ended! Your start is " + start + " and your end is " + end);
+    // System.out.println("Your start value is " + data[start] + " and your end value is " + data[end]);
   }
 
   public int size(){
@@ -124,9 +124,9 @@ public class MyDeque<E>{
 
 
   public void addFirst(E element){
-    System.out.println("Size is " + size);
-    System.out.println("data.length is " + data.length);
-    System.out.println("Attempted added element is " + element);
+    // System.out.println("Size is " + size);
+    // System.out.println("data.length is " + data.length);
+    // System.out.println("Attempted added element is " + element);
     if(element == null)
     throw new NullPointerException();
     else if(size == 0)
@@ -137,9 +137,9 @@ public class MyDeque<E>{
     }
     else
     {
-      System.out.println("My array before first resize is " + this.toString());
+      // System.out.println("My array before first resize is " + this.toString());
       resize();
-      System.out.println("My array after the first resize but before adding first is " + this.toString());
+      // System.out.println("My array after the first resize but before adding first is " + this.toString());
       if(start != 0)
       {
         start --;
@@ -152,16 +152,16 @@ public class MyDeque<E>{
         data[start] = element;
         size ++;
       }
-      System.out.println("My array after the first resize and after adding first is " + this.toString());
+      // System.out.println("My array after the first resize and after adding first is " + this.toString());
       resize();
-      System.out.println("My array after the second resize and after adding first is " + this.toString());
+      // System.out.println("My array after the second resize and after adding first is " + this.toString());
     }
   }
 
   public void addLast(E element){
-    System.out.println("Size is " + size);
-    System.out.println("data.length is " + data.length);
-    System.out.println("Attempted added element is " + element);
+    // System.out.println("Size is " + size);
+    // System.out.println("data.length is " + data.length);
+    // System.out.println("Attempted added element is " + element);
     if(element == null)
     throw new NullPointerException();
     else if(size == 0)
@@ -173,9 +173,9 @@ public class MyDeque<E>{
     }
     else
     {
-      System.out.println("My array before first resize is " + this.toString());
+      // System.out.println("My array before first resize is " + this.toString());
       resize();
-      System.out.println("My array after the first resize but before adding last is " + this.toString());
+      // System.out.println("My array after the first resize but before adding last is " + this.toString());
       if(end != data.length - 1)
       {
         end ++;
@@ -188,9 +188,9 @@ public class MyDeque<E>{
         data[end] = element;
         size ++;
       }
-      System.out.println("My array after the first resize and after adding last is " + this.toString());
+      // System.out.println("My array after the first resize and after adding last is " + this.toString());
       resize();
-      System.out.println("My array after the second resize and after adding last is " + this.toString());
+      // System.out.println("My array after the second resize and after adding last is " + this.toString());
     }
   }
   public E removeFirst(){
@@ -257,7 +257,12 @@ public class MyDeque<E>{
       MyDeque<Integer> thing = new MyDeque<Integer>(3);
       for(int i = 0; i < 4; i ++)
       {
-        thing.addLast(i);
+        thing.addFirst(i);
+        System.out.println("My thing is now " + Arrays.toString(thing.data));
+        System.out.println("My start index is now " + thing.start);
+        System.out.println("My end index is now " + thing.end);
+        System.out.println(thing.getFirst());
+        System.out.println(thing.getLast());
         System.out.println();
       }
     }
